0fc01a21fffee7df21674ffe0f1335eace0a9c50
Pippin Williamson <pippin@pippinsplugins.com>
2013-07-12 09:08:14 -0500

    Adding first files

---
+++ edd-terms-per-product.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * Plugin Name: Easy Digital Downloads - Terms Per Product
+ * Plugin URI: http://easydigitaldownloads.com/extension/terms-per-product
+ * Description: Allow terms of use to be specified on a per-product basis
+ * Author: Pippin Williamson
+ * Author URI: http://pippinsplugins.com
+ * Version: 1.0
+ * Text Domain: edd-terms-per-product
+ * Domain Path: languages
+*/
+
+class EDD_Terms_Per_Product {
+
+	function __construct() {
+
+		add_action( 'init', array( $this, 'load_textdomain' ) );
+		add_action( 'edd_meta_box_fields', array( $this, 'metabox_field' ), 200 );
+		add_action( 'edd_purchase_form_after_cc_form', array( $this, 'product_terms' ) );
+		add_action( 'edd_checkout_error_checks', array( $this, 'error_checks' ), 10, 2 );
+
+		add_filter( 'edd_metabox_fields_save', array( $this, 'fields_to_save' ) );
+		add_filter( 'edd_metabox_save__edd_download_terms', array( $this, 'sanitize_terms_save' ) );
+
+	}
+
+
+	public function load_textdomain() {
+		// Set filter for plugin's languages directory
+		$lang_dir = dirname( plugin_basename( __FILE__ ) ) . '/languages/';
+		$lang_dir = apply_filters( 'edd_terms_per_product_lang_directory', $lang_dir );
+
+		// Traditional WordPress plugin locale filter
+		$locale        = apply_filters( 'plugin_locale',  get_locale(), 'edd-terms-per-product' );
+		$mofile        = sprintf( '%1$s-%2$s.mo', 'edd-terms-per-product', $locale );
+
+		// Setup paths to current locale file
+		$mofile_local  = $lang_dir . $mofile;
+		$mofile_global = WP_LANG_DIR . '/edd-terms-per-product/' . $mofile;
+
+		if ( file_exists( $mofile_global ) ) {
+			// Look in global /wp-content/languages/edd folder
+			load_textdomain( 'edd-terms-per-product', $mofile_global );
+		} elseif ( file_exists( $mofile_local ) ) {
+			// Look in local /wp-content/plugins/easy-digital-downloads/languages/ folder
+			load_textdomain( 'edd-terms-per-product', $mofile_local );
+		} else {
+			// Load the default language files
+			load_plugin_textdomain( 'edd-terms-per-product', false, $lang_dir );
+		}
+	}
+
+
+	public function metabox_field( $post_id = 0 ) {
+		$terms = get_post_meta( $post_id, '_edd_download_terms', true );
+		?>
+		<p>
+			<strong><?php _e( 'Download Terms of Use', 'edd-terms-per-product' ); ?></strong>
+		</p>
+		<p>
+			<textarea name="_edd_download_terms" id="edd_download_terms" rows="10" cols="50" class="large-text"><?php echo $terms; ?></textarea>
+			<label for="edd_download_terms"><?php _e( 'Enter the terms of use for this product.', 'edd-terms-per-product' ); ?></label>
+		</p>
+		<?php
+	}
+
+	public function fields_to_save( $fields = array() ) {
+		$fields[] = '_edd_download_terms';
+		return $fields;
+	}
+
+	public function sanitize_terms_save( $data ) {
+		return wp_kses( $data, array(
+			'a' => array(
+				'href' => array(),
+				'title' => array()
+			),
+			'br' => array(),
+			'em' => array(),
+			'strong' => array()
+			)
+		);
+	}
+
+
+	public function product_terms() {
+		$cart_items = edd_get_cart_contents();
+		echo '<script type="text/javascript">jQuery(document).ready(function($){$("body").on("click", ".edd_per_product_terms_links", function(e) {e.preventDefault();var terms = $(this).attr("href");var parent = $(this).parent();$(terms).slideToggle();parent.find("a").toggle();});});</script>';
+		echo '<fieldset id="edd_terms_agreement">';
+		foreach ( $cart_items as $key => $item ) {
+			$terms = get_post_meta( $item['id'], '_edd_download_terms', true );
+			if( ! empty( $terms ) ) { ?>
+				<div id="edd-<?php echo $item['id']; ?>-terms-wrap">
+					<div id="edd_<?php echo $item['id']; ?>_terms" style="display:none;">
+						<?php echo wpautop( $terms ); ?>
+					</div>
+					<div id="edd_show_<?php echo $item['id']; ?>_terms">
+						<a href="#edd_<?php echo $item['id']; ?>_terms" class="edd_per_product_terms_links"><?php printf( __( 'Show Terms For %s', 'edd' ), get_post_field( 'post_title', $item['id'] ) ); ?></a>
+						<a href="#edd_<?php echo $item['id']; ?>_terms" class="edd_per_product_terms_links" style="display:none;"><?php _e( 'Hide Terms', 'edd' ); ?></a>
+					</div>
+					<label for="edd_agree_to_terms_<?php echo $item['id']; ?>">Agree to Terms</label>
+					<input name="edd_agree_to_terms_<?php echo $item['id']; ?>" class="required" type="checkbox" id="edd_agree_to_terms_<?php echo $item['id']; ?>" value="1"/>
+				</div>
+				<?php
+			}
+		}
+		echo '</fieldset>';
+	}
+
+	public function error_checks( $valid_data = array(), $post_data = array() ) {
+		$cart_items = edd_get_cart_contents();
+		foreach ( $cart_items as $key => $item ) {
+			if( ! isset( $post_data['edd_agree_to_terms_' . $item['id'] ] ) ) {
+				edd_set_error( 'agree_to_product_terms', __( 'You must agree to the terms of use for all products.', 'edd-terms-per-product' ) );
+			}
+		}
+	}
+
+}
+
+// Instantiate the class
+$edd_terms_per_product = new EDD_Terms_Per_Product;
---
+++ readme.txt
@@ -0,0 +1,42 @@
+=== Easy Digital Downloads - Terms Per Product ===
+Author URI: http://pippinsplugins.com
+Plugin URI: http://easydigitaldownloads.com/extension/terms-per-product
+Contributors: mordauk
+Donate link: http://pippinsplugins.com/support-the-site
+Tags: download, downloads, e-store, eshop, easy digital downloads, digital downloads, e-downloads, ecommerce, e commerce, e-commerce, selling, wp-ecommerce, wp ecommerce, mordauk, Pippin Williamson, pippinsplugins
+Requires at least: 3.3
+Tested up to: 3.6
+
+Stable Tag: 1.0
+License: GNU Version 2 or Any Later Version
+
+Allow terms of use to be specified on a per-product basis
+
+== Description ==
+
+Allow terms of use to be specified on a per-product basis for Easy Digital Downloads.
+
+This is an extension for [Easy Digital Downloads](http://wordpress.org/extend/plugins/easy-digital-downloads/) and does not function on its own.
+
+== Installation ==
+
+1. Activate the plugin
+2. Add the Terms of Use for each product from the Download edit screen
+
+== Screenshots ==
+
+1. Download products overview
+2. Download configuration
+3. Download configuration details
+4. Download configuration with variable prices
+5. Payment history
+6. Discount codes
+7. Earnings and sales reports
+8. Add to cart / purchase button
+9. Checkout screen
+
+== Changelog ==
+
+= 1.0: July 11, 2013 =
+
+* First offical release!
